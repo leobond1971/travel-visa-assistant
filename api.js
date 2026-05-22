fe419ac866dd83b3c59710a421db8defcdc20cab
leobond1971 <132722430+leobond1971@users.noreply.github.com>
2026-05-23 01:40:06 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
  const express = require('express');
  const cors = require('cors');
  const fetch = require('node-fetch');
-const path = require('path');
+ const path = require('path');
 
 const app = express();
 app.use(cors());
